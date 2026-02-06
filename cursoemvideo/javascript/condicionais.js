630f342fc818769247837048564928971109c89a
tarcisiodesales-lang <tarcisiodesales@gmail.com>
2026-02-06 12:49:12 -0300

    fiz desafios e aprendi condicional

---
+++ cursoemvideo/javascript/condicionais.js
@@ -0,0 +1,14 @@
+var vel = 78.5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60){ //Condicional simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija com resposabilidade.')
+
+var pais = 'BR'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'BR'){
+    console.log('Você é Brasileiro')
+}else{
+    console.log('Você é extrangeiro')
+}
