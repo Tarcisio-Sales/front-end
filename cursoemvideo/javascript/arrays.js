bdb721be5eb1fda0f6481c3153b813ddaa45f678
tarcisiodesales-lang <tarcisiodesales@gmail.com>
2026-02-08 07:12:49 -0300

    trabalhei com arrays

---
+++ cursoemvideo/javascript/arrays.js
@@ -0,0 +1,22 @@
+let vet = [8,5,3]
+/*
+vet[3] = 6 // acrescenta em uma posição específica
+vet.push(9) // acrescenta na última posição
+vet.length // tamanho do vetor
+vet.sort() // coloca em ordem crescente
+vet.indexOf(7) // procura no vetor em qual posição se encontra o valor 7; se nao encontrar, retorna -1
+console.log(vet.indexOf(8))
+
+//versão extendida
+for (var c = 0 ; c<vet.length ; c++){
+ 
+    console.log(vet[c])
+}
+*/
+//ECMAscript
+
+
+
+// for(var c in vet){
+//     console.log(vet[c])
+// }
